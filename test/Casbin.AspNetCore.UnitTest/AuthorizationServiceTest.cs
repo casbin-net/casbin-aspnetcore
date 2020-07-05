49443694c0666e358fe092fe18ea8fb13f3170d7
Sagilio <Sagilio@outlook.com>
2020-07-05 20:42:21 +0800

    Add basic tests for the core feature

---
+++ test/Casbin.AspNetCore.UnitTest/AuthorizationServiceTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Casbin.AspNetCore.Abstractions;
+using Casbin.AspNetCore.Policy;
+using Casbin.AspNetCore.UnitTest.Fixtures;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Casbin.AspNetCore.UnitTest
+{
+    public class AuthorizationServiceTest : IClassFixture<TestServerFixture>
+    {
+        private readonly IAuthorizationService _authorizationService;
+        private readonly ICasbinAuthorizationContextFactory _casbinAuthorizationContextFactory;
+
+        public AuthorizationServiceTest(TestServerFixture testServerFixture)
+        {
+            var services = testServerFixture.TestServer.Services;
+            _authorizationService = services.GetRequiredService<IAuthorizationService>();
+            _casbinAuthorizationContextFactory = services.GetRequiredService<ICasbinAuthorizationContextFactory>();
+        }
+
+        [Fact]
+        public async Task ShouldBasicAuthorizeAsync()
+        {
+            // alice
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, "alice")
+            }));
+
+            var casbinContext = _casbinAuthorizationContextFactory.CreateContext(user,
+                new CasbinAuthorizationData
+                {
+                    ResourceName = "data1", ActionName = "write"
+                });
+            var result = await _authorizationService
+                .AuthorizeAsync(user, casbinContext, new CasbinAuthorizationRequirement());
+            Assert.False(result.Succeeded);
+
+            casbinContext = _casbinAuthorizationContextFactory.CreateContext(user,
+                new CasbinAuthorizationData
+                {
+                    ResourceName = "data1", ActionName = "read"
+                });
+            result = await _authorizationService
+                .AuthorizeAsync(user, casbinContext, new CasbinAuthorizationRequirement());
+            Assert.True(result.Succeeded);
+
+            // bob
+            user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, "bob")
+            }));
+
+            casbinContext = _casbinAuthorizationContextFactory.CreateContext(user,
+                new CasbinAuthorizationData
+                {
+                    ResourceName = "data2", ActionName = "write"
+                });
+            result = await _authorizationService
+                .AuthorizeAsync(user, casbinContext, new CasbinAuthorizationRequirement());
+            Assert.True(result.Succeeded);
+
+            
+            casbinContext = _casbinAuthorizationContextFactory.CreateContext(user,
+                new CasbinAuthorizationData
+                {
+                    ResourceName = "data2", ActionName = "read"
+                });
+            result = await _authorizationService
+                .AuthorizeAsync(user, casbinContext, new CasbinAuthorizationRequirement());
+            Assert.False(result.Succeeded);
+        }
+    }
+}
---
+++ test/Casbin.AspNetCore.UnitTest/Utilities/TestUntility.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Casbin.AspNetCore.UnitTest.Utilities
+{
+    public static class TestUtility
+    {
+        public static string GetExampleFile(string fileName)
+        {
+            return Path.Combine("Examples", fileName);
+        }
+    }
+}
