b847518798cb29df78680464766ba47327ad4143
AsakusaRinne <asakusarinne@gmail.com>
2022-09-03 11:23:12 +0800

    feat: Add sample for blazor webassembly.
    (cherry picked from commit f287870e9aa43120d0b092ef760967890aaeb4a9)

---
+++ samples/BlazorWebAssemblySample/Server/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Casbin.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebAssemblySample.Server.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class ApiController : Controller
+    {
+
+        [HttpGet("index")]
+        [CasbinAuthorize]
+        public IActionResult Index()
+        {
+            return new JsonResult(new
+            {
+                Message = "You passed the casbin authorize."
+            });
+        }
+    }
+}
---
+++ samples/BlazorWebAssemblySample/Server/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Casbin.AspNetCore.Authorization;
+using Casbin.AspNetCore.Authorization.Transformers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.Extensions.Logging;
+using WebApplicationSample.Models;
+
+namespace BlazorWebAssemblySample.Server.Controllers
+{
+    [Route("Casbin")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("AttributeTest")]
+        [CasbinAuthorize]
+        public string AttribRouteTest(string tenantId)
+        {
+            return "You have passed the authentication test.";
+        }
+
+        [HttpGet("BasicTest")]
+        [CasbinAuthorize(nameof(BasicTest), nameof(System.Net.Http.HttpMethod.Get))]
+        public string BasicTest()
+        {
+            return "You have passed the authentication test.";
+        }
+    }
+}
